f53936eb00508d6d0b42a19fa4b20567b587bcfe
nikpatil2123 <nikpatil2123@gmail.com>
2026-08-25 16:35:16 +0530

    ecosystem uypdated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,22 +2,24 @@ module.exports = {
   apps: [
     {
       name: 'ticket-backend',
-      script: 'npm',
-      args: 'run start:prod',
+      script: 'node',
+      args: 'dist/src/main.js',
       cwd: './backend',
       env: {
         NODE_ENV: 'production',
         PORT: 3001,
+        HOSTNAME: '0.0.0.0',
       },
     },
     {
       name: 'ticket-frontend',
-      script: 'npm',
-      args: 'start',
+      script: 'node',
+      args: '.next/standalone/server.js',
       cwd: './frontend',
       env: {
         NODE_ENV: 'production',
         PORT: 3000,
+        HOSTNAME: '0.0.0.0',
       },
     },
   ],
